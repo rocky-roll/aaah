56d7532ec16a128cc9aa2fdd1f07551fdaa25961
rocky-roll <you@example.com>
2023-02-18 10:25:40 -0300

    first commit

--- recognize_google_.py
+++ recognize_google_.py
@@ -1,32 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# 
+#
 #    author='@rocky_roll,
 #    license='GNU GPLv3'
-# 
+#
 
 import queue
 import random
+
 import speech_recognition as sr
 from playsound import playsound
 
 listener = sr.Recognizer()
 
-filename = ('cat1.mp3','cat5.mp3')
+filename = ('cat1.mp3', 'cat5.mp3')
 
 cont = 0
-cont1= 0
+cont1 = 0
 q = queue.Queue()
 q1 = queue.Queue()
 
 
 def _sonido():
     global cont1
-    cont1 +=1
+    cont1 += 1
     q1.put_nowait(cont)
     numeros = random.randint(0, 1)
     playsound(filename[numeros])
+
 
 def listen():
     try:
@@ -56,11 +58,11 @@ def main():
 
             if "macri" in response or "Macri" in response:
                 _sonido()
-                cont +=1
+                cont += 1
                 q.put_nowait(cont)
-        except:          
+        except:
             pass
-    
+
+
 if __name__ == "__main__":
     main()
-
